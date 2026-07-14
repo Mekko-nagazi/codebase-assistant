fc26ee0c3971e155e4237ec79effa0ae71735471
Ivan Schetcko <meliodasochek9@gmail.com>
2026-07-14 17:11:14 +0300

    feat: validate ask path

--- src/commands/ask.ts
+++ src/commands/ask.ts
@@ -1,4 +1,6 @@
 import { Command } from "commander";
+import fs from "fs";
+import path from "path"; 
 
 // Описываем тип опций, которые приходят в команду ask
 type AskOptions = {
@@ -15,9 +17,46 @@ export function registerAskCommand(program: Command): void {
     .action(handleAsk);
 }
 
-//Обработчик команды ask
+// Валидатор пути
+function validatePath(projectPath: string): string {
+
+  // Проверка на то чтобы путь не был пустым
+  if (!projectPath || projectPath.trim() === "") {
+    throw new Error("Path is empty")
+  }
+
+  const resolvedPath = path.resolve(projectPath);
+  const baseDir = process.cwd();
+  const resolvedBaseDir = path.resolve(baseDir);
+  const safeBase = resolvedBaseDir + path.sep;
+
+  // Проверка на то чтобы путь был в рабочей лиректории включительно
+  if (!resolvedPath.startsWith(safeBase) && !(resolvedPath === resolvedBaseDir)) {
+    throw new Error("The path is outside the working directory.")
+  }
+
+  // Проверка на то чтобы путь существовал
+  if (!fs.existsSync(resolvedPath)) {
+    throw new Error("Path does not exist")
+  }
+
+  return resolvedPath
+}
+
+// Обработчик команды ask
 function handleAsk(question: string, options: AskOptions):
 void {
-  console.log("Question:", question);
-  console.log("Project path:", options.path);
+  try {
+    const projectPath = validatePath(options.path)
+
+    console.log("Question:", question);
+    console.log("Project path:", projectPath);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      return;
+    }
+
+    console.error("Unknown error");
+  }
 }
